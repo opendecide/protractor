fa6ad5cbc46fe24e3bc6b26347e12b22fa54948c
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2020-01-10 11:52:13 +0100

    Add built folder for yarn installation

---
+++ built/index.d.ts
@@ -0,0 +1,28 @@
+import { ElementHelper, ProtractorBrowser } from './browser';
+import { ElementArrayFinder, ElementFinder } from './element';
+import { ProtractorExpectedConditions } from './expectedConditions';
+import { ProtractorBy } from './locators';
+import { Ptor } from './ptor';
+export { Actions, Browser, Builder, Button, Capabilities, Capability, error, EventEmitter, FileDetector, Key, logging, promise, Session, until, WebDriver, WebElement, WebElementPromise } from 'selenium-webdriver';
+export { ElementHelper, ProtractorBrowser } from './browser';
+export { Config } from './config';
+export { ElementArrayFinder, ElementFinder } from './element';
+export { ProtractorExpectedConditions } from './expectedConditions';
+export { Locator, ProtractorBy } from './locators';
+export { PluginConfig, ProtractorPlugin } from './plugins';
+export { Ptor } from './ptor';
+export { Runner } from './runner';
+export declare const utils: {
+    firefox: any;
+    http: any;
+    remote: any;
+};
+export { Command, Name as CommandName } from 'selenium-webdriver/lib/command';
+export declare let protractor: Ptor;
+export declare let browser: ProtractorBrowser;
+export declare let $: (search: string) => ElementFinder;
+export declare let $$: (search: string) => ElementArrayFinder;
+export declare let element: ElementHelper;
+export declare let By: ProtractorBy;
+export declare let by: ProtractorBy;
+export declare let ExpectedConditions: ProtractorExpectedConditions;
